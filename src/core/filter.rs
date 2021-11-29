997685f19ac9e4ecf980ec662adc6047724a71c4
PepcyCh <pepsich86@163.com>
2021-11-29 09:28:14 +0800

    remove unused files

--- src/core/filter.rs
+++
@@ -1,5 +0,0 @@
-pub trait Filter: Send + Sync {
-    fn radius(&self) -> i32;
-
-    fn weight(&self, x: f32, y: f32) -> f32;
-}
--- src/core/material.rs
+++
@@ -1,5 +0,0 @@
-use crate::core::{intersection::Intersection, scatter::Scatter};
-
-pub trait Material: Send + Sync {
-    fn scatter(&self, inter: &Intersection<'_>) -> Box<dyn Scatter>;
-}
